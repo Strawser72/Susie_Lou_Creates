1b3599dd6349d6795e1802ae4454f8bb4ebcf157
David Strawser <hdstrawser@gmail.com>
2024-04-09 22:41:06 -0400

    created spring boot file

---
+++ Susie Lou Creates spring boot/Susie-Lou-Creates/src/main/java/com/example/Susie/Lou/Creates/SusieLouCreatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Susie.Lou.Creates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusieLouCreatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusieLouCreatesApplication.class, args);
+	}
+
+}
---
+++ Susie Lou Creates spring boot/Susie-Lou-Creates/src/test/java/com/example/Susie/Lou/Creates/SusieLouCreatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Susie.Lou.Creates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusieLouCreatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
